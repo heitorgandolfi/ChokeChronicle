f40577b0a7b632e84281fced1ad8d00a4db9bcd9
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2023-12-04 19:13:47 -0300

    feat: add default theme colors

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,17 @@
+export const defaultTheme = {
+  "primary-lighter": "#B3B9DF",
+  "primary-light": "#888EBC",
+  primary: "#393F70",
+  "primary-dark": "#262A50",
+  "primary-darker": "#1F2240",
+
+  secondary: "#77DD5D",
+
+  "gray-400": "#D7D5D5",
+  "gray-350": "#E6E8F6",
+  "gray-300": "#E6E5E5",
+  "gray-200": "#EDEDED",
+  "gray-150": "#F3F2F2",
+  "gray-100": "#FAFAFA",
+  white: "#FFFFFF",
+};
