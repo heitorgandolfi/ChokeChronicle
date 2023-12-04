153294fa300c4940cba0b849bf7be333dafcaa78
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2023-12-04 19:16:37 -0300

    refactor: include routes in App component and add ThemeProvider

--- App.tsx
+++ App.tsx
@@ -1,21 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from "styled-components/native";
+
+import { Routes } from "./src/routes";
+import { defaultTheme } from "./src/styles/themes/default";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Text>First commit</Text>
-    </View>
+    <ThemeProvider theme={defaultTheme}>
+      <Routes />
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
