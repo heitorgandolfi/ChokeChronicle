bbabc5b37bb199f6c71baee2c29907fde1cc5ac8
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2023-12-23 16:10:16 -0300

    feat: create New Workout form

---
+++ src/screens/newWorkout/newWorkoutForm.tsx
@@ -0,0 +1,79 @@
+import * as Yup from "yup";
+import { useForm } from "react-hook-form";
+import { useTheme } from "styled-components";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useNavigation } from "@react-navigation/native";
+
+import { MoodContainerFormFields } from "./formComponents/mood";
+import { FormContainer, SubmitButton, SubmitButtonText } from "./styles";
+import { BeltsContainerFormFields } from "./formComponents/belts";
+import { InteractionsContainerFormFields } from "./formComponents/interactions";
+import { LocationDateContainerFormFields } from "./formComponents/locationDate";
+
+const schema = Yup.object().shape({
+  trainingDate: Yup.string().required("Date is required"),
+  trainingLocation: Yup.string().required("Location is required"),
+  whiteBelt: Yup.string(),
+  blueBelt: Yup.string(),
+  purpleBelt: Yup.string(),
+  brownBelt: Yup.string(),
+  blackBelt: Yup.string(),
+  rolls: Yup.string(),
+  rests: Yup.string(),
+  subs: Yup.string(),
+  taps: Yup.string(),
+  mood: Yup.string().required("At least one must be selected"),
+});
+
+export const NewWorkoutForm = () => {
+  const theme = useTheme();
+  const navigation = useNavigation();
+
+  const {
+    control,
+    handleSubmit,
+    setValue,
+    reset,
+    watch,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const selectedImage = watch("mood");
+
+  const userHandleSubmit = (formData: any) => {
+    const { trainingDate, trainingLocation } = formData;
+    console.log(`form: ${JSON.stringify(formData)}`);
+
+    reset();
+
+    navigation.navigate("Home");
+  };
+
+  return (
+    <FormContainer>
+      <LocationDateContainerFormFields
+        control={control}
+        setValue={setValue}
+        errors={errors}
+        theme={theme}
+      />
+
+      <BeltsContainerFormFields control={control} setValue={setValue} errors={errors} />
+
+      <InteractionsContainerFormFields control={control} setValue={setValue} errors={errors} />
+
+      <MoodContainerFormFields
+        control={control}
+        setValue={setValue}
+        errors={errors}
+        selectedImage={selectedImage}
+      />
+
+      <SubmitButton activeOpacity={0.9} onPress={handleSubmit(userHandleSubmit)}>
+        <SubmitButtonText>Submit</SubmitButtonText>
+      </SubmitButton>
+    </FormContainer>
+  );
+};
---
+++ src/screens/newWorkout/styles.ts
@@ -0,0 +1,79 @@
+import styled from "styled-components/native";
+
+export const FormContainer = styled.View`
+  margin: 24px 16px;
+  padding: 20px 28px;
+
+  border: 1px solid transparent;
+  border-radius: 8px;
+
+  background: ${(props) => props.theme.primaryB};
+`;
+
+interface InputProps {
+  interactionInput?: boolean;
+  foughtBeltsInput?: boolean;
+  hasError?: boolean;
+  isHidden?: boolean;
+}
+
+export const Input = styled.TextInput<InputProps>`
+  display: ${(props) => (props.isHidden ? "none" : "flex")};
+
+  width: ${(props) => (props.interactionInput ? "32px" : "auto")};
+  margin-bottom: ${(props) => (props.foughtBeltsInput ? "none" : "12px")};
+  padding: 2px 8px;
+
+  text-align: ${(props) => (props.interactionInput || props.foughtBeltsInput ? "center" : "left")};
+
+  border-radius: 8px;
+  border: 1px solid;
+  border-color: ${(props) => (props.hasError ? props.theme.error : "transparent")};
+
+  color: ${(props) => props.theme.white};
+  background: ${(props) => props.theme.primaryA};
+`;
+
+export const ErrorText = styled.Text.attrs(() => ({ maxFontSizeMultiplier: 1 }))`
+  font-weight: bold;
+
+  margin: -8px 0 8px;
+
+  color: ${(props) => props.theme.error};
+`;
+
+interface FormTextProps {
+  interactionsText?: boolean;
+}
+
+export const FormText = styled.Text.attrs(() => ({ maxFontSizeMultiplier: 1 }))`
+  font-size: 16px;
+  font-weight: ${(props: FormTextProps) => (props.interactionsText ? "400" : "600")};
+
+  margin-bottom: 8px;
+
+  color: ${(props) => props.theme.white};
+`;
+
+export const SubmitButton = styled.TouchableOpacity`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 90%;
+  height: 42px;
+
+  margin: 16px auto 0;
+  padding: 9px 18px;
+
+  border: 1px solid transparent;
+  border-radius: 16px;
+
+  background-color: ${(props) => props.theme.tertiary};
+  color: white;
+`;
+
+export const SubmitButtonText = styled.Text.attrs(() => ({ maxFontSizeMultiplier: 1 }))`
+  color: ${(props) => props.theme["gray-450"]};
+  font-weight: 600;
+`;
