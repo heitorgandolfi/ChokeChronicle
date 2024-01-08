f951f00bc4a5272f603ef3595ca48791867dd828
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2024-01-08 18:30:18 -0300

    refactor: change models props

--- src/models/newWorkoutFormFieldsModel/index.ts
+++ src/models/newWorkoutFormFieldsModel/index.ts
@@ -1,6 +1,21 @@
 import { Control, FieldErrors, UseFormSetValue } from "react-hook-form";
 
-type NewWorkoutFormFieldsProps = {
+export type NewWorkoutFormFieldsProps = {
+  trainningDate: string;
+  trainningLocation: string;
+  whiteBelt: string;
+  blueBelt: string;
+  purpleBelt: string;
+  brownBelt: string;
+  blackBelt: string;
+  rolls: string;
+  rests: string;
+  subs: string;
+  taps: string;
+  mood: string;
+};
+
+export type NewWorkoutFormWithOptionalFieldsProps = {
   trainningDate: string;
   trainningLocation: string;
   whiteBelt?: string;
@@ -16,7 +31,7 @@ type NewWorkoutFormFieldsProps = {
 };
 
 export interface FormFieldsProps {
-  control: Control<NewWorkoutFormFieldsProps>;
-  setValue: UseFormSetValue<NewWorkoutFormFieldsProps>;
-  errors: FieldErrors<NewWorkoutFormFieldsProps>;
+  control: Control<NewWorkoutFormWithOptionalFieldsProps>;
+  setValue: UseFormSetValue<NewWorkoutFormWithOptionalFieldsProps>;
+  errors: FieldErrors<NewWorkoutFormWithOptionalFieldsProps>;
 }
--- src/models/trainningCardModel/index.ts
+++ src/models/trainningCardModel/index.ts
@@ -2,15 +2,21 @@ export interface TrainningCardProps {
   date: string;
   city: string;
   mood: string;
-  rolls: number;
-  belts: {
-    white: number;
-    blue: number;
-    purple: number;
-    brown: number;
-    black: number;
-  };
-  rests: number;
-  subs: number;
-  taps: number;
+  rolls: string;
+  whiteBelt: string;
+  blueBelt: string;
+  purpleBelt: string;
+  brownBelt: string;
+  blackBelt: string;
+  rests: string;
+  subs: string;
+  taps: string;
+}
+
+export interface BeltsProps {
+  whiteBelt: string;
+  blueBelt: string;
+  purpleBelt: string;
+  brownBelt: string;
+  blackBelt: string;
 }
