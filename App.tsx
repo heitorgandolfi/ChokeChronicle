e1d039a54a492322a12b46d7b1464903f038e50c
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2024-01-10 18:15:43 -0300

    feat: add font loading and handle fonts loading in App component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,45 @@
+import { ActivityIndicator, StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components/native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+import {
+  Roboto_300Light,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 
 import { Routes } from "./src/routes";
 import { defaultTheme } from "./src/styles/themes/default";
 
 export default function App() {
+  const [LoadFonts] = useFonts({
+    Roboto_300Light,
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  const handleFontsLoading = () => {
+    if (!LoadFonts) {
+      return (
+        <ActivityIndicator
+          style={{ flex: 1, backgroundColor: defaultTheme["primary-dark"] }}
+          color={defaultTheme.secondary}
+        />
+      );
+    }
+
+    return <Routes />;
+  };
+
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Routes />
-    </ThemeProvider>
+    <>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <ThemeProvider theme={defaultTheme}>{handleFontsLoading()}</ThemeProvider>
+      </GestureHandlerRootView>
+      <StatusBar backgroundColor="transparent" translucent />
+    </>
   );
 }
