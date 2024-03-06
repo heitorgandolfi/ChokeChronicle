d52f54944b39accf0f86784d3a56430040ac09b5
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2024-03-06 15:31:03 -0300

    feat: add new colors to default theme

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -8,9 +8,20 @@ export const defaultTheme = {
   "primary-darker": "#1F2240",
 
   secondary: "#77DD5D",
+  "secondary-dark": "#66bb6a",
+
   tertiary: "#6A59FF",
 
   error: "#FB5353",
+
+  "yellow-500": "#ffca28",
+
+  "blue-450": "#42a5f5",
+  "blue-550": "#4e73e4",
+
+  "purple-500": "#a159e4",
+
+  "brown-600": "#8B735E",
 
   "gray-450": "#E3E6FF",
   "gray-400": "#D7D5D5",
@@ -19,7 +30,10 @@ export const defaultTheme = {
   "gray-200": "#EDEDED",
   "gray-150": "#F3F2F2",
   "gray-100": "#FAFAFA",
+  "gray-50": "#F9F9F9",
+
   white: "#FFFFFF",
+  black: "#0e0d0d",
 
   fonts: {
     Light: "Roboto_300Light",
