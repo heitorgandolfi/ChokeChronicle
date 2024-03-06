c22c4e677aa7e0a0402fbef661c5b944436198a9
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2024-03-06 15:43:48 -0300

    feat: add ChartComponent and its styles

---
+++ src/screens/chart/components/renderChart/index.tsx
@@ -0,0 +1,79 @@
+import { View } from "react-native";
+import { VictoryPie } from "victory-native";
+
+import { colorSchemes } from "../../../../shared/utils/chartColorScheme";
+import { ChartDataProps } from "../../../../stores/getTrainningsStore/chartDataTrainnings/getChartDataState";
+
+import { SelectedChartCategory } from "../..";
+
+import { MetricsCard, MetricsCardPercentage, MetricsCardText } from "./styles";
+
+interface ChartComponentProps {
+  chartData: ChartDataProps[];
+  totalData: number;
+  selectedCategory: SelectedChartCategory;
+  selectedCategoryInfo: string;
+  handleCardOnPress: (detailsLabel: string) => void;
+}
+
+export const ChartComponent = ({
+  chartData,
+  totalData,
+  selectedCategory,
+  selectedCategoryInfo,
+  handleCardOnPress,
+}: ChartComponentProps) => {
+  const renderChart = () => {
+    return (
+      <VictoryPie
+        key={selectedCategory}
+        data={chartData}
+        innerRadius={90}
+        labelRadius={1000000000}
+        colorScale={colorSchemes[selectedCategory]}
+        style={{
+          data: {
+            fillOpacity: ({ datum }) =>
+              datum.label === selectedCategoryInfo || selectedCategoryInfo === "" ? 1 : 0.2,
+          },
+        }}
+      />
+    );
+  };
+
+  const renderPercentageCondition = (item: ChartDataProps) => {
+    if (selectedCategory === "belts") {
+      return (
+        <MetricsCardPercentage>
+          {`(${((item.y / totalData) * 100).toFixed(1)}%)`}
+        </MetricsCardPercentage>
+      );
+    }
+    return null;
+  };
+
+  const renderMetricsCard = (item: ChartDataProps, index: number) => {
+    return (
+      <MetricsCard
+        key={index}
+        leftBorderColor={colorSchemes[selectedCategory][index]}
+        onPress={() => handleCardOnPress(item.label)}
+        activeOpacity={0.9}
+      >
+        <MetricsCardText>{item.label}</MetricsCardText>
+
+        <View style={{ flexDirection: "row", alignItems: "center", gap: 4 }}>
+          <MetricsCardText>{item.y}</MetricsCardText>
+          {renderPercentageCondition(item)}
+        </View>
+      </MetricsCard>
+    );
+  };
+
+  return (
+    <>
+      {renderChart()}
+      {chartData.map((item, index) => renderMetricsCard(item, index))}
+    </>
+  );
+};
---
+++ src/screens/chart/components/renderChart/styles.ts
@@ -0,0 +1,40 @@
+import styled from "styled-components/native";
+
+interface MetricsCardProps {
+  leftBorderColor: string;
+}
+export const MetricsCard = styled.TouchableOpacity<MetricsCardProps>`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  align-self: center;
+
+  width: 80%;
+  height: 50px;
+  margin-bottom: 20px;
+
+  elevation: 4;
+
+  border-left-width: 12px;
+  border-radius: 8px;
+  border-color: ${({ leftBorderColor }) => leftBorderColor};
+  background: ${({ theme }) => theme.white};
+`;
+
+export const MetricsCardText = styled.Text.attrs(() => ({ maxFontSizeMultiplier: 1 }))`
+  font-size: 18px;
+  font-family: ${({ theme }) => theme.fonts.Medium};
+
+  text-transform: capitalize;
+
+  color: ${({ theme }) => theme["primary-darker"]};
+`;
+
+export const MetricsCardPercentage = styled.Text.attrs(() => ({ maxFontSizeMultiplier: 1 }))`
+  font-size: 18px;
+  font-family: ${({ theme }) => theme.fonts.Medium};
+
+  text-transform: capitalize;
+
+  color: ${({ theme }) => theme["primary-light"]};
+`;
